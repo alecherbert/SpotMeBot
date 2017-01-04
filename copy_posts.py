512493d2ef9f8125c7297183406d649c32c65ff9
Alec <herbert.alec@gmail.com>
2017-01-04 03:50:46 -0500

    fixed uneven arguments count error.  copy_posts.py now uses threads to copy from 3 different subreddits simultaneously

--- copy_posts.py
+++ copy_posts.py
@@ -1,19 +1,31 @@
 import praw
+import threading
 from praw_test import getSubreddit, getPRAW
 from handle import handle
 
+domains = ['youtube.com','m.youtube.com','youtu.be']
+subs_to_copy = ['posthardcore','metalcore','listentothis']
+
 def main():
-    domains = ['youtube.com','m.youtube.com','youtu.be']
     r = getPRAW();
-    r_posthardcore = getSubreddit(r,'posthardcore')
     r_spotmebot = getSubreddit(r,'spotmebot')
-    for s in r_posthardcore.stream.submissions():
+    threads = []
+    for sub in subs_to_copy:
+        t = threading.Thread(target=copyFromSubreddit, args=(sub,r_spotmebot))
+        threads.append(t)
+        t.start()
+
+def copyFromSubreddit(from_sub_name,to_sub):
+    r = getPRAW();
+    from_sub = getSubreddit(r,from_sub_name)
+    for s in from_sub.stream.submissions():
         if s.domain in domains:
             post_url = s.url
             post_title = s.title
-            post = submitLinkPost(post_title,post_url,r_spotmebot)
+            post = submitLinkPost(post_title,post_url,to_sub,from_sub_name)
 
-def submitLinkPost(title,link,subreddit):
+def submitLinkPost(title,link,subreddit,from_text):
+    title += " - from /r/"+from_text
     submission = subreddit.submit(title=title,url=link)
     return submission
 
